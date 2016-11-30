500513509f606948a6fe84610203ebd803a17521
Harrison McGuire <hmcguire13@gmail.com>
2016-11-30 11:42:50 -0500

    carousels

--- assets/js/scripttwo.js
+++ assets/js/scripttwo.js
@@ -13,32 +13,32 @@
 			className: 'itemtwo'
 		}
 
-		var carousel = $('.carouseltwo').CircularCarousel(optionstwo);
+		var carouseltwo = $('.carouseltwo').CircularCarousel(optionstwo);
 
 		/* Fires when an item is about to start it's activate animation */
-		carousel.on('itemBeforeActive', function(e, item) {
+		carouseltwo.on('itemBeforeActive', function(e, item) {
 			$(item).css('box-shadow', '0 0 20px blue');
 		});
 
 		/* Fires after an item finishes it's activate animation */
-		carousel.on('itemActive', function(e, item) {
+		carouseltwo.on('itemActive', function(e, item) {
 			$(item).css('box-shadow', '0 0 20px green');
 		});
 
 		/* Fires when an active item starts it's de-activate animation */
-		carousel.on('itemBeforeDeactivate', function(e, item) {
+		carouseltwo.on('itemBeforeDeactivate', function(e, item) {
 			$(item).css('box-shadow', '0 0 20px yellow');
 		});
 
 		/* Fires after an active item has finished it's de-activate animation */
-		carousel.on('itemAfterDeactivate', function(e, item) {
+		carouseltwo.on('itemAfterDeactivate', function(e, item) {
 			$(item).css('box-shadow', '');
 		});
 
 		/* Manaully click an item anywhere in the carousel */
 		$('.carouseltwo .itemtwo').click(function(e) {
 			var index = $(this).index('li');
-			carousel.cycleActiveTo(index);
+			carouseltwo.cycleActiveTo(index);
 			e.preventDefault();
 		});
 	
